bd1eb5d7e28bfa101e8f79fcc7b481c3d9697e13
Adam Comerford <adamc@10gen.com>
2014-09-26 16:22:57 +0100

    removed provisioner commands, not needed

--- vagrant/m202-ubuntu1404/provisioners/setup.sh
+++ vagrant/m202-ubuntu1404/provisioners/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-set -e
+#set -e
 
-HOME="/home/m202"
-COURSE="m202"
-MONGOPROC="$HOME/mongoProc"
+#HOME="/home/m202"
+#COURSE="m202"
+#MONGOPROC="$HOME/mongoProc"
 
-echo 'Setting up VM...'
+#echo 'Setting up VM...'
 
 #echo 'Updating system... this may take a while'
 #apt-get -y update > /dev/null 2>&1
